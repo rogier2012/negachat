7e16ac37dae56e85d9812799b2017921740e7b5a
Gijs <Gijs@LAPTOP-GIJS>
2015-04-08 14:36:37 +0200

    Verzenden en lezen mogelijk!

--- NegaChat/src/multicast/SocketGekloot.java
+++ NegaChat/src/multicast/SocketGekloot.java
@@ -31,7 +31,7 @@ public class SocketGekloot {
 	
 	public void nogmeerGekloot() throws IOException {
 		// join a Multicast group and send the group salutations
-		 String msg = "Hello RON!:)";
+		 String msg = "Hello:)";
 		 InetAddress group = InetAddress.getByName("228.5.6.7");
 		 MulticastSocket s = new MulticastSocket(6789);
 		
@@ -40,15 +40,17 @@ public class SocketGekloot {
 		                             group, 6789);
 		 s.send(hi);
 		 System.out.println("sent hi");
-		 // get their responses!
+//		 // get their responses!
 		 byte[] buf = new byte[1000];
 		 DatagramPacket recv = new DatagramPacket(buf, buf.length);
 		 s.receive(recv);
 		 
 		 
-		 System.out.println("String: " + recv.toString());
+		 System.out.println("recv.toString(): " + recv.toString());
 		 System.out.println("getdata.tostring: " + recv.getData().toString());
 		 System.out.println("recv.getdata.length: " + recv.getData().length);
+		 String received = new String(recv.getData(), 0, recv.getLength());
+		 System.out.println("final string: " + received);
  		 
 		 // OK, I'm done talking - leave the group...
 //		 s.leaveGroup(group);
