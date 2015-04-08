1551057f27c22887968238dfd87278f368334a40
rogier2012 <r.monshouwer@student.utwente.nl>
2015-04-08 14:29:48 +0200

    Nog een keer

--- NegaChat/src/negachat/view/NegaView.java
+++ NegaChat/src/negachat/view/NegaView.java
@@ -50,31 +50,6 @@ public class NegaView {
 		
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		frame.getContentPane().add(tabbedPane, BorderLayout.CENTER);
-		
-		JPanel panel_1 = new JPanel();
-		tabbedPane.addTab("Group", null, panel_1, null);
-		GridBagLayout gbl_panel_1 = new GridBagLayout();
-		gbl_panel_1.columnWidths = new int[]{429, 0, 0};
-		gbl_panel_1.rowHeights = new int[]{232, 0, 0};
-		gbl_panel_1.columnWeights = new double[]{1.0, 0.0, Double.MIN_VALUE};
-		gbl_panel_1.rowWeights = new double[]{1.0, 0.0, Double.MIN_VALUE};
-		panel_1.setLayout(gbl_panel_1);
-		
-		JTextPane txtpnText = new JTextPane();
-		txtpnText.setText("text");
-		GridBagConstraints gbc_txtpnText = new GridBagConstraints();
-		gbc_txtpnText.fill = GridBagConstraints.BOTH;
-		gbc_txtpnText.insets = new Insets(0, 0, 0, 5);
-		gbc_txtpnText.gridx = 0;
-		gbc_txtpnText.gridy = 1;
-		panel_1.add(txtpnText, gbc_txtpnText);
-		
-		JButton btnSend = new JButton("SEND");
-		GridBagConstraints gbc_btnSend = new GridBagConstraints();
-		gbc_btnSend.fill = GridBagConstraints.HORIZONTAL;
-		gbc_btnSend.gridx = 1;
-		gbc_btnSend.gridy = 1;
-		panel_1.add(btnSend, gbc_btnSend);
 	}
 
 }
