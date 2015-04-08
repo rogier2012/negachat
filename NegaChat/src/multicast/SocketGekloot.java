bb3c2619934852e786e933c5eef20519f7cc78ee
Gijs <Gijs@LAPTOP-GIJS>
2015-04-08 11:41:15 +0200

    2

--- NegaChat/src/multicast/SocketGekloot.java
+++ NegaChat/src/multicast/SocketGekloot.java
@@ -11,10 +11,19 @@ public class SocketGekloot {
 	MulticastSocket socket;
 	
 	public SocketGekloot()	{
-		 try {
-			socket = new MulticastSocket(6789);
+//		 try {
+//			socket = new MulticastSocket(6789);
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+	}
+	
+	public static void main(String[] args) {
+		SocketGekloot sock = new SocketGekloot();
+		try {
+			sock.nogmeerGekloot();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
@@ -24,18 +33,20 @@ public class SocketGekloot {
 	public void nogmeerGekloot() throws IOException {
 		// join a Multicast group and send the group salutations
 		 String msg = "Hello";
-		 InetAddress group = InetAddress.getByName("228.5.6.7");
-		 MulticastSocket s = new MulticastSocket(6789);
+		 InetAddress group = InetAddress.getByName("192.168.5.1");
+		 MulticastSocket s = new MulticastSocket();
+		 System.out.println(group);
+		 System.out.println("s.getPort(): " + s.getPort());
 		 s.joinGroup(group);
 		 DatagramPacket hi = new DatagramPacket(msg.getBytes(), msg.length(),
 		                             group, 6789);
 		 s.send(hi);
-		 // get their responses!
-		 byte[] buf = new byte[1000];
-		 DatagramPacket recv = new DatagramPacket(buf, buf.length);
-		 s.receive(recv);
-		 // OK, I'm done talking - leave the group...
-		 s.leaveGroup(group);
+//		 // get their responses!
+//		 byte[] buf = new byte[1000];
+//		 DatagramPacket recv = new DatagramPacket(buf, buf.length);
+//		 s.receive(recv);
+//		 // OK, I'm done talking - leave the group...
+//		 s.leaveGroup(group);
 	}
 	
 }
