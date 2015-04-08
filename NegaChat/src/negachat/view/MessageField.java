d27d03f7d3f53f125ddf5e418dc43ac12af1120f
chris <christiaanvdbogaard@gmail.com>
2015-04-08 15:49:24 +0200

    MessageFieldController + view toegevoegd
    aarszweet

--- NegaChat/src/negachat/view/MessageField.java
+++ NegaChat/src/negachat/view/MessageField.java
@@ -1,14 +1,37 @@
 package negachat.view;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class MessageField extends JPanel {
+	
+	
+	private JButton btnSend;
+	private JTextArea txtInput;
 
 	/**
 	 * Create the panel.
 	 */
 	public MessageField() {
+		btnSend = new JButton("Send");
+	    txtInput = new JTextArea();
 
+		
+		
+	}
+	
+	public JButton getButton(){
+		return btnSend;
+	}
+	
+	public JTextArea getTxtInput(){
+		return txtInput;
 	}
 
 }
---
+++ NegaChat/src/negachat/view/MessageFieldController.java
@@ -0,0 +1,22 @@
+package negachat.view;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MessageFieldController {
+	private MessageField mField;
+	private ActionListener actionListener;
+	
+	public MessageFieldController(MessageField mField){
+		this.mField = mField;
+	}
+	
+	public void contol(){        
+        actionListener = new ActionListener() {
+              public void actionPerformed(ActionEvent actionEvent) {                  
+//                  linkBtnAndLabel();
+              }
+        };                
+        mField.getButton().addActionListener(actionListener);   
+    }
+}
