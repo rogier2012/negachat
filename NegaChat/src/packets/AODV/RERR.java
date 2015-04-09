4d46ff239b2c4efb415ba2911dac29a27aa4508a
Ron van Bree <ronvbree@hotmail.com>
2015-04-09 15:25:27 +0200

    test

---
+++ NegaChat/src/packets/AODV/RERR.java
@@ -0,0 +1,12 @@
+package packets.AODV;
+
+import packets.Packet;
+
+public class RERR extends Packet {
+
+	public RERR(String destination, String source) {
+		super(destination, source);
+		// TODO Auto-generated constructor stub
+	}
+
+}
