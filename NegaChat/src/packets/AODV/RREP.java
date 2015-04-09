70974141c8fc155cf6449b1455aaf74ca6e934e9
Ron van Bree <ronvbree@hotmail.com>
2015-04-09 10:52:38 +0200

    geen message in contructor packet

--- NegaChat/src/packets/AODV/RREP.java
+++ NegaChat/src/packets/AODV/RREP.java
@@ -5,7 +5,7 @@ import packets.Packet;
 public class RREP extends Packet {
 
 	public RREP(String destination, String source, String message) {
-		super(destination, source, message);
+		super(destination, source);
 		// TODO Auto-generated constructor stub
 	}
 
