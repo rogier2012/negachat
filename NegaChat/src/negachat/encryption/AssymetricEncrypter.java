d9111c4544092c21e78a8b55bdbbd825c6b5bcaa
chris <christiaanvdbogaard@gmail.com>
2015-04-10 12:18:56 +0200

    Encryption

---
+++ NegaChat/src/negachat/encryption/AssymetricEncrypter.java
@@ -0,0 +1,95 @@
+package negachat.encryption;
+
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+
+public class AssymetricEncrypter {
+	Cipher RSACipher;
+	PrivateKey privateKey;
+	PublicKey publicKey;
+	private final String ALGORITHM = "RSA";
+
+	public AssymetricEncrypter() {
+		generateKeys();
+	}
+
+	public void generateKeys() {
+
+		KeyPairGenerator keyGen;
+		try {
+			keyGen = KeyPairGenerator.getInstance(ALGORITHM);
+			keyGen.initialize(1024);
+			KeyPair keyPair = keyGen.generateKeyPair();
+			privateKey = keyPair.getPrivate();
+			publicKey = keyPair.getPublic();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public void createCipher() {
+
+		try {
+			RSACipher = Cipher.getInstance(ALGORITHM);
+
+		} catch (Exception e) {
+		}
+
+	}
+
+	public byte[] Encrypt(byte[] message) {
+		byte[] EncryptedMessage = null;
+		try {
+			RSACipher.init(Cipher.ENCRYPT_MODE, publicKey);
+			try {
+				EncryptedMessage = RSACipher.doFinal(message);
+			} catch (IllegalBlockSizeException | BadPaddingException e) {
+				e.printStackTrace();
+			}
+		} catch (InvalidKeyException e) {
+		}
+		return EncryptedMessage;
+	}
+
+	public byte[] Decrypt(byte[] message) {
+		byte[] DecryptedMessage = null;
+		try {
+			RSACipher.init(Cipher.DECRYPT_MODE, privateKey);
+			try {
+				DecryptedMessage = RSACipher.doFinal(message);
+			} catch (IllegalBlockSizeException | BadPaddingException e) {
+				e.printStackTrace();
+			}
+		} catch (InvalidKeyException e) {
+		}
+		return DecryptedMessage;
+	}
+
+	public byte[] Encrypt(byte[] message, PublicKey pubKey) {
+		byte[] EncryptedMessage = null;
+		try {
+			RSACipher.init(Cipher.ENCRYPT_MODE, pubKey);
+			try {
+				EncryptedMessage = RSACipher.doFinal(message);
+			} catch (IllegalBlockSizeException | BadPaddingException e) {
+				e.printStackTrace();
+			}
+		} catch (InvalidKeyException e) {
+		}
+		return EncryptedMessage;
+	}
+
+	public PublicKey getKey() {
+		return publicKey;
+	}
+
+}
