853daf4aea717346d86278d782d4e8c3d94bb58c
Ron van Bree <ronvbree@hotmail.com>
2015-04-10 10:41:15 +0200

    .

--- NegaChat/src/negachat/packets/MessagePacket.java
+++ NegaChat/src/negachat/packets/MessagePacket.java
@@ -18,7 +18,7 @@ public class MessagePacket extends Packet {
 	public MessagePacket(String destination, String source) {
 		super(source);
 		this.source = source;
-		this.destination = destination;
+		this.destination = destination; 
 		setType(TYPE);
 	}
 	
