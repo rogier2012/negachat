fa1873550279aa0a095f5a77bc2431e38cfefbaa
Ron van Bree <ronvbree@hotmail.com>
2015-04-10 10:16:36 +0200

    packet

---
+++ NegaChat/src/negachat/packets/Packet.java
@@ -0,0 +1,60 @@
+package negachat.packets;
+
+public abstract class Packet {
+	
+	/*
+	 * AODV PACKETS TYPE IDENTIFIER BYTES:
+	 * 
+	 * 00000000 - Normal
+	 * 00000001 - HELLO
+	 * 00000010 - RREQ
+	 * 00000011 - RREP
+	 * 00000100 - RERR
+	 * 00000101 - GroupMessage
+	 */
+	
+	/*
+	 * Instance Variables
+	 */
+	
+	private byte type;
+	private String source;
+	
+	/*
+	 * Constructors
+	 */
+	
+	public Packet(String source)	{
+		this.source = source;
+	}
+	
+	/*
+	 * Queries
+	 */
+	
+	public abstract Packet convertToPacket(byte[] byteArray);
+	
+	public abstract byte[] toByteArray();
+
+	/*
+	 * Getters and Setters
+	 */
+	
+	public byte getType() {
+		return type;
+	}
+
+	public void setType(byte type) {
+		this.type = type;
+	}
+
+	public String getSource() {
+		return source;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+	
+	
+}
