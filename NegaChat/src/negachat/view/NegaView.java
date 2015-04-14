e6aac3c492e4e54b0e8fd5cf50984eb26caef65c
Ron van Bree <ronvbree@hotmail.com>
2015-04-14 11:53:26 +0200

    updates!

--- NegaChat/src/negachat/view/NegaView.java
+++ NegaChat/src/negachat/view/NegaView.java
@@ -57,18 +57,18 @@ public class NegaView {
 	 * @wbp.parser.entryPoint
 	 */
 	public NegaView() {
-//		myName = JOptionPane.showInputDialog(frame,"Please enter a nickname between 3 and 14 characters", null);
-//		if (myName != null){
-//			while (myName.length() > 14|| myName.length() < 4){
-//				if(myName.length() < 4){
-//					myName = JOptionPane.showInputDialog(frame,"Please use a nickname with more than 3 characters", null);
-//				} else{
-//					myName = JOptionPane.showInputDialog(frame,"Please don't use more than 14 characters", null);
-//				}
-//				
-//			} 
+		myName = JOptionPane.showInputDialog(frame,"Please enter a nickname between 3 and 14 characters", null);
+		if (myName != null){
+			while (myName.length() > 14|| myName.length() < 4){
+				if(myName.length() < 4){
+					myName = JOptionPane.showInputDialog(frame,"Please use a nickname with more than 3 characters", null);
+				} else{
+					myName = JOptionPane.showInputDialog(frame,"Please don't use more than 14 characters", null);
+				}
+				
+			} 
 		initialize();
-//		}
+		}
 	}
 
 	/**
