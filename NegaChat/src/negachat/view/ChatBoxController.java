11cd197461ba3da84b38cf459d808849a10bdabe
Ron van Bree <ronvbree@hotmail.com>
2015-04-15 10:45:54 +0200

    test sound

--- NegaChat/src/negachat/view/ChatBoxController.java
+++ NegaChat/src/negachat/view/ChatBoxController.java
@@ -1,17 +1,26 @@
 package negachat.view;
 
+import negachat.audio.AudioPlayer;
+
 public class ChatBoxController {
 	private ChatBox chatBox;
 	
+	private AudioPlayer audioPlayer;
+	private static final String NOTIFIER = "Notifier.wav";
+	
 	public ChatBoxController(ChatBox chatbox){
 		this.chatBox = chatbox;
+		audioPlayer = new AudioPlayer();
 	}
 
 	public void setMessage(String message){
 		chatBox.append(message);
+		audioPlayer.play(NOTIFIER);
 		chatBox.repaint();
 	}
 
-	
+	public void playSound(String file){
+		
+	}
 	
 }
