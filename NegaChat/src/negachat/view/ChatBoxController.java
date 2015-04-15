667aa583ddc739b297deca20b7a75d02ff221bf9
Ron van Bree <ronvbree@hotmail.com>
2015-04-15 11:00:12 +0200

    wololo?

--- NegaChat/src/negachat/view/ChatBoxController.java
+++ NegaChat/src/negachat/view/ChatBoxController.java
@@ -4,10 +4,10 @@ import negachat.audio.AudioPlayer;
 
 public class ChatBoxController {
 	private ChatBox chatBox;
-	
 	private AudioPlayer audioPlayer;
 	private static final String NOTIFIER = "Notifier.wav";
-	private static final String WOLOLO = "Wololo.wav";
+	private static final String WOLOLO = "30_Wololo.wav";
+	private static final String HERB_LAUGH = "11_Herb_laugh.wav";
 	
 	public ChatBoxController(ChatBox chatbox){
 		this.chatBox = chatbox;
@@ -16,9 +16,11 @@ public class ChatBoxController {
 
 	public void setMessage(String message){
 		chatBox.append(message);
-		
 		if (message.charAt(0) == 3 && message.charAt(1) == 0)	{
 			playSound(WOLOLO);
+			
+		} else if(message.charAt(0) == 1 && message.charAt(1) == 1)	{
+			playSound(HERB_LAUGH);
 		} else	{
 			playSound(NOTIFIER);
 		}
