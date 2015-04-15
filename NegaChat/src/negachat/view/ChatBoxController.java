27582adc2e445e75bee5bc878d95527d9697d7ee
Ron van Bree <ronvbree@hotmail.com>
2015-04-15 11:31:29 +0200

    fix?

--- NegaChat/src/negachat/view/ChatBoxController.java
+++ NegaChat/src/negachat/view/ChatBoxController.java
@@ -5,7 +5,7 @@ import negachat.audio.AudioPlayer;
 public class ChatBoxController {
 	private ChatBox chatBox;
 	private AudioPlayer audioPlayer;
-	private static final String NOTIFIER = "Notifier.wav";
+	private static final String NOTIFIER = "Notification.wav";
 	private static final String WOLOLO = "30_Wololo.wav";
 	private static final String HERB_LAUGH = "11_Herb_laugh.wav";
 	
@@ -16,10 +16,9 @@ public class ChatBoxController {
 
 	public void setMessage(String message){
 		chatBox.append(message);
-		if (message.charAt(0) == 3 && message.charAt(1) == 0)	{
+		if (message.charAt(0) == '3' && message.charAt(1) == '0')	{
 			playSound(WOLOLO);
-			
-		} else if(message.charAt(0) == 1 && message.charAt(1) == 1)	{
+		} else if(message.charAt(0) == '1' && message.charAt(1) == '1')	{
 			playSound(HERB_LAUGH);
 		} else	{
 			playSound(NOTIFIER);
